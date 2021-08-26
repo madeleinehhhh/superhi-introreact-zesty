6061df243b4ebcade7f3ae693e5c14b395a235b9
madeleine <75395506+madeleinehhhh@users.noreply.github.com>
2021-08-25 23:41:26 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
+
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
+
+const images = [cabbage, mango, fig, gaze, peach, avocado];
 
 const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+      /*if (currentImage < length) {
+        return currentImage + 1;
+      } else {
+        return 0;
+      }*/
+    });
+  };
+
   return (
-    <div className="App">
-      <div class="title">
+    <main>
+      <header>
         <h1>Zesty</h1>
         <h2>
           A photography project
           <br />
           by Ella Fielding
         </h2>
-      </div>
-      <div class="image-container">
-        <img src="" alt="" />
-      </div>
-    </div>
+      </header>
+
+      <figure>
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        <img src={images[currentImage]} onClick={handleClick} alt="" />
+      </figure>
+    </main>
   );
 };
 
